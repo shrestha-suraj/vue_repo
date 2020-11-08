817f19752d1adf192c14f6c51d718d1f8cd3c118
Suraj Shrestha <sshrestha@LT-C02DH0X4MD6N.local>
2020-11-08 13:28:09 -0600

    Added boilerplate code for easy setup on projects, added project 4 and added more directives on the file

---
+++ boiler-plate/index.js
@@ -0,0 +1,12 @@
+new Vue({
+    el:"#root",
+    data:{
+
+    },
+    methods:{
+
+    },
+    computed:{
+        
+    }
+})
---
+++ project-4/index.js
@@ -0,0 +1,16 @@
+new Vue({
+    el:"#root",
+    data:{
+        ingredients:['meat','fruit','cookies'],
+        persons:[
+            {name:'Max',age:27,color:'red'},
+          {name:'Anna',age:'unknown',color:'blue'}
+        ]
+    },
+    methods:{
+
+    },
+    computed:{
+        
+    }
+})
